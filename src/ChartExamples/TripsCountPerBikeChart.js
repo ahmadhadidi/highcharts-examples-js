114c370e9413316e99d0b1bc3fcecc050ab4b718
Ahmad Hadidi <ahmadhadidi@gmail.com>
2020-08-08 23:20:02 +0200

    Fixed trips count per bike chart.

--- src/ChartExamples/TripsCountPerBikeChart.js
+++ src/ChartExamples/TripsCountPerBikeChart.js
@@ -7,28 +7,44 @@ export const TripsCountPerBikeChart = (bikeData) => {
     const [data, setData] = useState(bikeData.bikeData);
     const [bikeId, setBikeId] = useState(21434);
 
-    // Filter *per* Bike ID
-    // const filteredTrips = data.filter((n) => {
-    //     return Object.keys(n.bike)
-    // })
-
+    // We select distinct objects by property value from an array of objects
+    // Resource: https://codeburst.io/javascript-array-distinct-5edc93501dc4
     const filteredTrips = Array.from(new Set(data.map(n => n.bike))).map(id => {
         return {
             id: id,
-            data: data.find( s => s.bike === id ),
-            count: data.reduce((count, n) => {return count = count + 1;}, 0)
+            tripData: data.filter(s => s.bike === id),
+            amountOfTrips: data.filter(s => s.bike === id).length
         }
     });
 
+    const xAxisCategories = filteredTrips.map(n => n.id);
+    const numberOfTripsPerBike = filteredTrips.map( n => n.tripData.length );
+
     console.log("filteredTrips", filteredTrips);
+    console.log("Amount of Trips per Bike", numberOfTripsPerBike);
+    console.log("xAxisCategories", xAxisCategories);
 
     // Format the series in a way that Highcharts understands
     // Resource -- https://www.youtube.com/watch?v=G3BS3sh3D8Q
     // Make sure that you adhere to creating a collection of arrays within 1 array.
+    // const ready = [{
+    //     name: "Trips Count",
+    //     data: numberOfTripsPerBike
+    // }]
+
     const ready = [{
-        name: "Trips Count",
-        data: [filteredTrips.length]
+        name: "Trip Count",
+        data: filteredTrips.map(n => [n.id, n.amountOfTrips])
     }]
+
+    //     filteredTrips.map(n => {
+    //     return {
+    //         name: "Bike IDs",
+    //         data: [n.id, n.amountOfTrips]
+    //     }
+    // });
+
+    console.log("ready", ready);
 
     // Make the chart object and pass our `formattedSeries` array to it.
     const chart = useMemo(() => {
@@ -40,7 +56,7 @@ export const TripsCountPerBikeChart = (bikeData) => {
                 text: `Ahmad AlHadidi`
             },
             chart: {
-                type: 'bar'
+                type: 'column'
             },
             yAxis: {
                 title: {
@@ -49,9 +65,14 @@ export const TripsCountPerBikeChart = (bikeData) => {
             },
             xAxis: {
                 title: {
-                    text: ``
+                    text: `Bike IDs`
                 },
-                categories: [`Bike #${bikeId}`],
+                type: 'category',
+                // style: {
+                //     fontSize: '13px',
+                //     fontFamily: 'Verdana, sans-serif'
+                // },
+                // categories: ready.map(n => n.name),
                 // I guess this is for visually impaired users.
                 accessibility: {
                     rangeDescription: 'Range: 2010 to 2017'
