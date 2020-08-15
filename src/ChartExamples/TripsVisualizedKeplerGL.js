a57e620471199e2f4605ff34e77b2db650b36fd6
Ahmad Hadidi <ahmadhadidi@gmail.com>
2020-08-15 23:58:46 +0200

    Automatically load configurations for the visualization. Fix an issue with time of trips not matching the timeline.

--- src/ChartExamples/TripsVisualizedKeplerGL.js
+++ src/ChartExamples/TripsVisualizedKeplerGL.js
@@ -12,6 +12,10 @@ import { taskMiddleware } from "react-palm/tasks";
 import { Provider, useDispatch } from "react-redux";
 import { addDataToMap } from "kepler.gl/actions";
 import { processGeojson } from 'kepler.gl/processors';
+import KeplerGlSchema from 'kepler.gl/schemas';
+
+// Kepler.GL Config
+import tripVisState from '../helpers/visStateForTrips';
 
 // Create a reducer for Kepler
 const reducers = combineReducers({
@@ -61,7 +65,7 @@ export const TripsVisualizedKeplerGL = (bikeData) => {
     // }
 
     // We process each trip with the format that Kepler.GL understands
-    // Refer to this image:
+    // Refer to this image: 'highcharts-examples-js/keplergl-research/tripExample.png'
     // Link: https://github.com/keplergl/kepler.gl/blob/master/examples/demo-app/src/data/sample-animate-trip-data.js
     const processedTrips = data.map(n => {
         // Create dedicated variables for coordinates and the time
@@ -69,8 +73,10 @@ export const TripsVisualizedKeplerGL = (bikeData) => {
         const end_lng = n.end_lng;
         const start_lat = n.start_lat;
         const end_lat = n.end_lat;
-        const start_datetime = new Date(n.start_datetime).getTime() / 1000;
-        const end_datetime = new Date(n.end_datetime).getTime() / 1000;
+
+        // Add 2 hours (7200 Seconds) to make the playback start at 02:01
+        const start_datetime = (new Date(n.start_datetime).getTime() / 1000) + 7200;
+        const end_datetime = (new Date(n.end_datetime).getTime() / 1000) + 7200;
 
         // Since Kepler.GL requires a minimum of 3 arrays containing the Lng, Lat, Elevation, and Time, we can
         // solve this by creating an average value of the aforementioned elements.
@@ -82,19 +88,22 @@ export const TripsVisualizedKeplerGL = (bikeData) => {
         // Since Javascript doesn't come with a customized way to output a certain formatting
         // we will use a library called Moment which will allow us to output timestamps
         // in a way that suits us.
-        const startEpoch = moment.unix(start_datetime);
-        const endEpoch = moment.unix(end_datetime);
+        // ! : Remove two hours because moment adds it.
+        const startEpoch = moment.unix(start_datetime).subtract(2, 'h');
+        const endEpoch = moment.unix(end_datetime).subtract(2, 'h');
         const tripDuration = endEpoch.diff(startEpoch, 'minutes');
 
         // Distance function
-        // https://www.geodatasource.com/developers/javascript
+        // Resource: https://www.geodatasource.com/developers/javascript
         const tripDistance = distance(
             start_lat, // pass start lat
             start_lng, // pass start lng
             end_lat, // pass end lat
             end_lng, // pass end lng
             "k"
-        ).toFixed(3);
+        ).toFixed(2);
+
+        // Calculate the trip speed
         const tripSpeed = (parseFloat(tripDistance) / (endEpoch.diff(startEpoch, 'minutes')) * 60);
 
         // We return each trip with the format that Kepler.GL requires for each trip as the following
@@ -105,11 +114,14 @@ export const TripsVisualizedKeplerGL = (bikeData) => {
                 "Start Time ▶️": startEpoch.format("DD/MM - HH:mm"),
                 "End Time 🏁": endEpoch.format("DD/MM - HH:mm"),
                 "Duration ⏱️": `${tripDuration} Min`,
+                "Distance 📏": `${tripDistance} KM`,
                 "Estimated Speed 🎿": `${tripSpeed.toFixed(1)} KM/h`,
+                // Note that Kepler hover only shows the first 5 properties so this one won't appear on hover
+                "raw_speed": tripSpeed.toFixed(1)
             },
             "geometry": {
                 "type": "LineString",
-                // MUST BE AT LEAST THREE POINTS..
+                // MUST BE AT LEAST THREE ARRAYS..
                 // LNG, LAT, Elevation, Epoch
                 "coordinates": [
                     [start_lng, start_lat, 0, start_datetime],  // Add the data for the start of the trip
@@ -150,8 +162,10 @@ export const TripsVisualizedKeplerGL = (bikeData) => {
                     datasets: {
                         info: {
                             label: "Trip Details",
+                            id: 'trips-layer',
                         },
-                        data: processGeojson(ready)
+                        data: processGeojson(ready),
+
                     },
                     option: {
                         centerMap: true,
@@ -160,13 +174,20 @@ export const TripsVisualizedKeplerGL = (bikeData) => {
                     config: {
                         "mapState": {
                             "bearing": 0,
-                            "dragRotate": false,
+                            "dragRotate": true,
                             "latitude": 50.933249957830746,
                             "longitude": 6.943077214828662,
                             "pitch": 30,
                             "zoom": 12.048058916916952,
                             "isSplit": false
                         },
+                        "mapStyle": {
+                            "styleType": "dark",
+                            "mapStyles": {}
+                        },
+                        "visState":
+                            tripVisState  // Configuration for the trips.
+
                     }
                 })
             );
@@ -185,3 +206,13 @@ export const TripsVisualizedKeplerGL = (bikeData) => {
         </Provider>
     );
 }
+
+// Journal:
+// DONE: Custom Color on the trips themselves (speed bound).
+// Can't be done since it is a bug.
+// ABNDND: Show legend by default.
+// Doesn't have its own trigger.
+// ABNDND: Show Coordinates by default.
+// Really annoying
+// DONE: Time of the trips doesn't match the timeline 🤔.
+// TODO: Inject the charts into KeplerGLs sidebar.
---
+++ src/helpers/animationConfiguration.js
@@ -0,0 +1,4 @@
+export default {
+    "currentTime": 1561941355852,
+    "speed": 0.003
+}
